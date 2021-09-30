2f1d3a1cbeb109d5cfc8c982c8b727e77bf9a030
RishabhSinghBhadoriya <91659537+RishabhSinghBhadoriya@users.noreply.github.com>
2021-09-30 13:43:55 +0530

    Create first.c
    program to convert celcius to fareniet

---
+++ first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int celcius,fareniet;
+    printf("enter values of temperature in celcius: ");
+    scanf("%d",&celcius);
+    fareniet = 1.8*celcius+32;
+    printf("the value in fareniet: %d\n", fareniet);
+}
